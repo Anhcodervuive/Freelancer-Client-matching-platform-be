879f89bd0fa95242aef8dfe793628591059766b4
Đỗ Thanh Dũ <97421974+DoThanhDuB2111834@users.noreply.github.com>
2025-09-26 14:15:43 +0700

    Add client freelancer detail API and saved job schema

--- src/routes/client-freelancer.route.ts
+++ src/routes/client-freelancer.route.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 
-import { listFreelancersForClient } from '~/controllers/client/freelancer.controller'
+import {
+        getFreelancerDetailForClient,
+        listFreelancersForClient
+} from '~/controllers/client/freelancer.controller'
 import authenticateMiddleware from '~/middlewares/authentication'
 import { errorHandler } from '~/utils/error-handler'
 
 const router = Router()
 
 router.get('/freelancers', authenticateMiddleware, errorHandler(listFreelancersForClient))
+router.get('/freelancers/:freelancerId', authenticateMiddleware, errorHandler(getFreelancerDetailForClient))
 
 export default router
