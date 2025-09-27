ae0545e40f7c5fd19a85e4a1d8ee37da5ff3a955
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-09-27 11:37:48 +0700

    Logging error

--- src/types/socket.io.d.ts
+++ src/types/socket.io.d.ts
@@ -1,9 +1,9 @@
 import type { User } from '../generated/prisma'
 
 declare module 'socket.io' {
-        interface SocketData {
-                user?: User
-        }
+	interface SocketData {
+		user?: User
+	}
 }
 
 export {}
